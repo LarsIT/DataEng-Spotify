68947497c41c2818bc9e952e641f44cbca586e5e
Lars <lars.bmnn@protonmail.com>
2023-10-06 00:11:30 +0200

    expanded feature on whole data set

--- data_pipeline/modify_album_data.py
+++ data_pipeline/modify_album_data.py
@@ -7,48 +7,51 @@ def remove_items(data:dict, *args:str):
     for keyword in args:
         data.pop(keyword)
 
-
-# adjust file path for a loop when logic complete
-
-# columns:
-# album_group, album_type, artist, spotify_id, name, release_date, number_of_tracks
-
-# file handling
-filepath = 'data_pipeline/0DjR09NBgtZbkOnBZays9o_albums.json'
-with open(filepath, 'r') as jsonfile:
-    data = json.load(jsonfile)
-    data = data['items']
-
-# modifiy data
-
 unwound_albums = []
-# Iterate through albums
+# adjust file path for a loop when logic complete
+for artists_albums in os.listdir('data/albums'):
 
-for album in data:
+    # columns:
+    # album_group, album_type, artist, spotify_id, name, release_date, number_of_tracks
 
-    # variables for ease of use
-    artists = album['artists']
-    artist_count = len(artists)
+    # file handling
+    filepath = f'data/albums/{artists_albums}'
+    with open(filepath, 'r') as jsonfile:
+        data = json.load(jsonfile)
+        data = data['items']
 
-    # remove unnecessary data points
-    remove_items(album, 'available_markets', 'external_urls', 'release_date_precision', 'uri', 'images', 'href')
+    # modifiy data
 
-    # duplicate entries for albums with multiple artists
-    for i in range(0, artist_count):
-        current_artist = album['artists'][i] # convenience variable
-        
-        new_album = album.copy() # create copy
-        new_album.pop('artists') # remove artists list
 
-        # set artist related values on higher level in hierarchy
-        new_album['artist_name'] = current_artist['name']
-        new_album['artist_spotify_id'] = current_artist['id']
-        new_album['artist_type'] = current_artist['type']
-        
-        # rename attribute
-        new_album['album_id'] = new_album['id']
-        unwound_albums.append(new_album)
+    # Iterate through albums
+
+    for album in data:
+
+        # variables for ease of use
+        artists = album['artists']
+        artist_count = len(artists)
+
+        # remove unnecessary data points
+        remove_items(album, 'available_markets', 'external_urls', 'release_date_precision', 'uri', 'images', 'href')
+
+        # duplicate entries for albums with multiple artists
+        for i in range(0, artist_count):
+            current_artist = album['artists'][i] # convenience variable
+            
+            new_album = album.copy() # create copy
+            new_album.pop('artists') # remove artists list
+
+            # set artist related values on higher level in hierarchy
+            new_album['artist_name'] = current_artist['name']
+            new_album['artist_spotify_id'] = current_artist['id']
+            new_album['artist_type'] = current_artist['type']
+            
+            # rename attribute
+            new_album['album_id'] = new_album['id']
+            unwound_albums.append(new_album)
     
 
 
 print(unwound_albums)
+
+# TODO move albums into dataframe
